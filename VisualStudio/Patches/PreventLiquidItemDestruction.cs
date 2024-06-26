2b1c673b5463a2ad6b0f5416de746904da721cef
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-06-26 18:00:05 -0300

    Update PreventLiquidItemDestruction.cs

--- VisualStudio/Patches/PreventLiquidItemDestruction.cs
+++ VisualStudio/Patches/PreventLiquidItemDestruction.cs
@@ -1,44 +1,49 @@
 ﻿namespace FuelManager
 {
-    using System;
-    using Il2Cpp;
-    using HarmonyLib;
-    using UnityEngine;
-    using Il2CppTLD.Gear;
+	using System;
+	using Il2Cpp;
+	using HarmonyLib;
+	using UnityEngine;
+	using Il2CppTLD.Gear;
+	using Il2CppTLD.IntBackedUnit;
 
-    internal static class PreventLiquidItemDestruction
-    {
-        internal static int deductLiquidFromInventoryCallDepth = 0;
+	public static class PreventLiquidItemDestruction
+	{
+		internal static int deductLiquidFromInventoryCallDepth = 0;
 
-        [HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.DeductLiquidFromInventory), new Type[] { typeof(float), typeof(LiquidType) })]
-        internal static class PlayerManager_DeductLiquidFromInventory
-        {
-            private static void Prefix()
-            {
-                deductLiquidFromInventoryCallDepth++;
-            }
-            private static void Postfix()
-            {
-                deductLiquidFromInventoryCallDepth--;
-            }
-        }
+		[HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.DeductLiquidFromInventory), new Type[] { typeof(ItemLiquidVolume), typeof(LiquidType) })]
+		public static class PlayerManager_DeductLiquidFromInventory
+		{
+			private static void Prefix()
+			{
+				deductLiquidFromInventoryCallDepth++;
+			}
+			private static void Postfix()
+			{
+				deductLiquidFromInventoryCallDepth--;
+			}
+		}
 
-        [HarmonyPatch(typeof(Inventory), nameof(Inventory.DestroyGear), new Type[] { typeof(GameObject) })]
-        internal static class Inventory_DestroyGear
-        {
-            private static bool Prefix(GameObject go)
-            {
-                if (deductLiquidFromInventoryCallDepth > 0)
-                {
-                    LiquidItem liquidItem = go.GetComponent<LiquidItem>();
+		[HarmonyPatch(typeof(Inventory), nameof(Inventory.DestroyGear), new Type[] { typeof(GameObject) })]
+		public static class Inventory_DestroyGear
+		{
+			// this prevents the patch from occuring if the var is not greater than 0
+			public static bool Prepare()
+			{
+				return deductLiquidFromInventoryCallDepth > 0;
+			}
 
-                    if (liquidItem != null && liquidItem.LiquidType == Main.GetKerosene())
-                    {
-                        return false;
-                    }
-                }
-                return true;
-            }
-        }
-    }
+			public static bool Prefix(GameObject go)
+			{
+				LiquidItem liquidItem = go.GetComponent<LiquidItem>();
+
+				if (liquidItem != null && liquidItem.LiquidType == Main.GetKerosene())
+				{
+					return false;
+				}
+
+				return true;
+			}
+		}
+	}
 }
