f1622f8656beb8bda4ec2cd91b38e56dd69b3792
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-12-12 21:07:29 -0400

    Create Extensions.cs

---
+++ VisualStudio/Utilities/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Il2CppTLD.UI;
+
+namespace FuelManager.Utilities
+{
+	public static class Extensions
+	{
+		public static bool IsPanelPatchable(this Panel_AutoReferenced panel)
+		{
+			return !(panel == null);
+		}
+	}
+}
