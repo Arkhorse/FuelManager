215062488e504af5dc9fddc3100452f15e1850a8
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-12-15 16:30:47 -0400

    Update PlayerManager_AddLiquidToInventory.cs

--- VisualStudio/Patches/PlayerManager_AddLiquidToInventory.cs
+++ VisualStudio/Patches/PlayerManager_AddLiquidToInventory.cs
@@ -3,25 +3,25 @@ using Il2CppTLD.IntBackedUnit;
 
 namespace FuelManager
 {
-    [HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.AddLiquidToInventory), [ typeof(ItemLiquidVolume), typeof(LiquidType) ])]
-    internal class PlayerManager_AddLiquidToInventory
-    {
-        private static void PostFix(PlayerManager __instance, ItemLiquidVolume litersToAdd, LiquidType liquidType, ref ItemLiquidVolume ___result)
-        {
-            if (__instance == null) return;
-            if (liquidType == null) return;
+	[HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.AddLiquidToInventory), [ typeof(ItemLiquidVolume), typeof(LiquidType) ])]
+	internal class PlayerManager_AddLiquidToInventory
+	{
+		private static void PostFix(PlayerManager __instance, ItemLiquidVolume litersToAdd, LiquidType liquidType, ref ItemLiquidVolume __result)
+		{
+			if (__instance == null) return;
+			if (liquidType == null) return;
 
-            Main.Logger.Log($"PlayerManager is not null, liquidType: {liquidType.ToString()}, litersToAdd: {litersToAdd.m_Units}L", FlaggedLoggingLevel.Debug);
+			Main.Logger.Log($"PlayerManager is not null, liquidType: {Localization.Get(liquidType.LiquidName.m_LocalizationID)}, litersToAdd: {litersToAdd.m_Units}L, Result: {__result.m_Units}L", FlaggedLoggingLevel.Debug);
 
-            if (!(liquidType == Main.GetKerosene())) return;
+			if (!(liquidType == Main.GetKerosene())) return;
 
-            if (___result != litersToAdd)
-            {
-                Message.SendLostMessageDelayed(litersToAdd - ___result);
+			if (__result != litersToAdd)
+			{
+				Message.SendLostMessageDelayed(litersToAdd - __result);
 
 				// just pretend we added everything, so the original method will not generate new containers
-				___result = litersToAdd;
-            }
-        }
-    }
+				__result = litersToAdd;
+			}
+		}
+	}
 }
