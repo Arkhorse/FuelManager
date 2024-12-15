a227901046a8eb28be4097883d20b1f22cc0b4c2
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-12-15 16:31:34 -0400

    Update Panel_Inventory_Examine_OnUnload.cs

--- VisualStudio/Patches/Panel_Inventory_Examine_OnUnload.cs
+++ VisualStudio/Patches/Panel_Inventory_Examine_OnUnload.cs
@@ -1,17 +1,27 @@
 ﻿namespace FuelManager
 {
-    [HarmonyPatch(typeof(Panel_Inventory_Examine), nameof(Panel_Inventory_Examine.OnUnload))]
-    internal class Panel_Inventory_Examine_OnUnload
-    {
-        private static bool Prefix(Panel_Inventory_Examine __instance)
-        {
+	[HarmonyPatch(typeof(Panel_Inventory_Examine), nameof(Panel_Inventory_Examine.OnUnload))]
+	internal class Panel_Inventory_Examine_OnUnload
+	{
+		private static bool Prefix(Panel_Inventory_Examine __instance)
+		{
 			if (!__instance.IsPanelPatchable()) return true;
 			if (__instance.m_GearItem == null) return true;
-			if (Fuel.IsFuelItem(__instance.m_GearItem))
-            {
-                Fuel.Drain(__instance.m_GearItem, __instance);
-            }
-            return true;
-        }
-    }
+
+			GearItem gi = __instance.m_GearItem;
+
+			try
+			{
+				if (Fuel.IsFuelItem(gi))
+				{
+					Fuel.Drain(gi, __instance);
+				}
+			}
+			catch (Exception e)
+			{
+				Main.Logger.Log($"OnUnload failed\n", FlaggedLoggingLevel.Exception, e);
+			}
+			return true;
+		}
+	}
 }
