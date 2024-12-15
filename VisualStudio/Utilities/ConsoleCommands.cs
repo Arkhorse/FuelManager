cfce0a9dacfcea43e813a14e0406f531968fd99e
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-12-15 16:30:40 -0400

    is to ==

--- VisualStudio/Utilities/ConsoleCommands.cs
+++ VisualStudio/Utilities/ConsoleCommands.cs
@@ -7,10 +7,10 @@ namespace FuelManager
             var inv = GameManager.GetInventoryComponent();
             for (int i = 0; i < inv.m_Items.Count; i++)
             {
-                if (inv.m_Items[i] is null) continue;
+                if (inv.m_Items[i] == null) continue;
                 GearItem gi = inv.m_Items[i];
 
-                if (gi is null) continue;
+                if (gi == null) continue;
 
                 if (CommonUtilities.NormalizeName(gi.name) == "GEAR_JerrycanRusty")
                 {
