4f3ac6e1cb7b1e5acb197feec45d8397954fdf85
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-12-15 16:31:27 -0400

    Update ItemDescriptionPage_CanExamine.cs

--- VisualStudio/Patches/ItemDescriptionPage_CanExamine.cs
+++ VisualStudio/Patches/ItemDescriptionPage_CanExamine.cs
@@ -1,16 +1,17 @@
 ﻿namespace FuelManager
 {
-    [HarmonyPatch(typeof(ItemDescriptionPage), nameof(ItemDescriptionPage.CanExamine), new Type[] { typeof(GearItem) })]
-    internal class ItemDescriptionPage_CanExamine
-    {
-        private static bool Prefix(GearItem gi, ref bool __result)
-        {
-            if (gi != null && Fuel.IsFuelItem(gi))
-            {
-                __result = true;
-                return false;
-            }
-            return true;
-        }
-    }
+	[HarmonyPatch(typeof(ItemDescriptionPage), nameof(ItemDescriptionPage.CanExamine), [typeof(GearItem)])]
+	internal class ItemDescriptionPage_CanExamine
+	{
+		private static bool Prefix(ref GearItem gi, ref bool __result)
+		{
+			if (gi == null) return true;
+			if (Fuel.IsFuelItem(gi))
+			{
+				__result = true;
+				return false;
+			}
+			return true;
+		}
+	}
 }
