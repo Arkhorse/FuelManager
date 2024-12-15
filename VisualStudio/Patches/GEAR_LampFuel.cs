e1453dc91be4e066293b8b27c17d2b012e1dc6c4
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-12-15 16:31:24 -0400

    rem patches temp as not possible
    This is the bulk issue of the patch. Adding components to vanilla items doesnt work

--- VisualStudio/Patches/GEAR_LampFuel.cs
+++ VisualStudio/Patches/GEAR_LampFuel.cs
@@ -1,20 +1,43 @@
 ﻿
-namespace FuelManager
-{
-    //[HarmonyPatch(typeof(GearItem), nameof(GearItem.Deserialize), new Type[] { typeof(GearItemSaveDataProxy) })]
-    [HarmonyPatch(typeof(GearItem), nameof(GearItem.Awake))]
-    public static class AddComponents_LampFuel
-    {
-        // "GEAR_LampFuel"
-        public static void Prefix(ref GearItem __instance)
-        {
-            if (__instance == null) return;
+//using System.Text;
 
-            if (__instance.name != null && CommonUtilities.NormalizeName(__instance.name) == "GEAR_LampFuel")
-            {
-                //FuelItemAPI.AddRepair(__instance, Constants.REPAIR_HARVEST_GEAR, new int[] { 1 }, Constants.REPAIR_TOOLS, "Play_RepairingMetal");
-                FuelItemAPI.AddHarvest(ref __instance, Constants.REPAIR_HARVEST_GEAR, new int[] { 2 }, Constants.HARVEST_TOOLS, "Play_HarvestingMetalSaw");
-            }
-        }
-    }
-}
+//namespace FuelManager
+//{
+//	//[HarmonyPatch(typeof(GearItem), nameof(GearItem.Deserialize), new Type[] { typeof(GearItemSaveDataProxy) })]
+//	[HarmonyPatch(typeof(GearItem), nameof(GearItem.Awake))]
+//	public static class AddComponents_LampFuel
+//	{
+//		// "GEAR_LampFuel"
+//		public static void Postfix(ref GearItem __instance)
+//		{
+//			if (__instance == null) return;
+//			if (string.IsNullOrWhiteSpace(__instance.name)) return;
+//			if (CommonUtilities.NormalizeName(__instance.name) == "GEAR_LampFuel")
+//			{
+//				Main.Logger.Log("Begin patch for GEAR_LampFuel", FlaggedLoggingLevel.Debug, LoggingSubType.IntraSeparator);
+//				StringBuilder sb = new StringBuilder();
+
+//				//FuelItemAPI.AddRepair(__instance, Constants.REPAIR_HARVEST_GEAR, new int[] { 1 }, Constants.REPAIR_TOOLS, "Play_RepairingMetal");
+
+//				bool harvest = __instance.gameObject.GetComponent<Harvest>() == null;
+
+//				sb.AppendLine($"Before attempting to add harvest component, harvest is null: {harvest}");
+
+//				try
+//				{
+//					FuelItemAPI.AddHarvest(ref __instance, Constants.REPAIR_HARVEST_GEAR, [2], Constants.HARVEST_TOOLS, "Play_HarvestingMetalSaw");
+//					sb.AppendLine($"Was able to add harvest");
+//				}
+//				catch (Exception e)
+//				{
+//					harvest = __instance.gameObject.GetComponent<Harvest>() == null;
+
+//					sb.AppendLine($"Was not able to add harvest");
+//					sb.AppendLine(e.Message);
+//				}
+//				sb.AppendLine($"Finally, the harvest component is null: {harvest}");
+//				Main.Logger.Log(sb.ToString(), FlaggedLoggingLevel.Debug);
+//			}
+//		}
+//	}
+//}
