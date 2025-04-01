f0448fc2c322afe61318046a5ad703a918340c00
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2025-04-01 14:37:34 -0300

    Update Buttons.cs

--- VisualStudio/Utilities/Buttons.cs
+++ VisualStudio/Utilities/Buttons.cs
@@ -1,92 +1,93 @@
 namespace FuelManager
 {
-    internal static class Buttons
-    {
-        internal static Panel_Inventory_Examine _Panel_Inventory_Examine = new();
-        internal static Vector3 GetBottomPosition(params Component[] components)
-        {
-            Vector3 result = new(0, 1000, 0);
+	internal static class Buttons
+	{
+#pragma warning disable CS8603
+		internal static Vector3 GetBottomPosition(params Component[] components)
+		{
+			Vector3 result = new(0, 1000, 0);
 
-            foreach (Component eachComponent in components)
-            {
-                if (eachComponent.gameObject.activeSelf && result.y > eachComponent.transform.localPosition.y)
-                {
-                    result = eachComponent.transform.localPosition;
-                }
-            }
+			try
+			{
+				foreach (Component eachComponent in components)
+				{
+					if (eachComponent.gameObject.activeSelf && result.y > eachComponent.transform.localPosition.y)
+					{
+						result = eachComponent.transform.localPosition;
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Main.Logger.Log($"Attempting to set the bottom position failed, the result is {result}, original is {{0, 1000, 0}}", FlaggedLoggingLevel.Exception, e);
+			}
+			return result;
+		}
 
-            return result;
-        }
+		internal static bool IsSelected(UIButton button)
+		{
+			Panel_Inventory_Examine_MenuItem menuItem = button.GetComponent<Panel_Inventory_Examine_MenuItem>();
+			if (menuItem == null) return false;
+			return menuItem.m_Selected;
+		}
 
-        internal static bool IsSelected(UIButton button)
-        {
-            Panel_Inventory_Examine_MenuItem? menuItem = button.GetComponent<Panel_Inventory_Examine_MenuItem>();
-            if (menuItem == null) return false;
-            return menuItem.m_Selected;
-        }
+		internal static void SetButtonLocalizationKey(UIButton button, string key) => SetButtonLocalizationKey(button.gameObject, key);
 
-        internal static void SetButtonLocalizationKey(UIButton button, string key) => SetButtonLocalizationKey(button?.gameObject, key);
+		internal static void SetButtonLocalizationKey(GameObject gameObject, string key)
+		{
+			if (gameObject != null)
+			{
+				bool wasActive = gameObject.activeSelf;
+				gameObject.SetActive(false);
 
-        internal static void SetButtonLocalizationKey(GameObject? gameObject, string key)
-        {
-            if (gameObject is not null)
-            {
-                bool wasActive = gameObject.activeSelf;
-                gameObject.SetActive(false);
+				UILocalize localize = gameObject.GetComponentInChildren<UILocalize>();
+				if (localize != null)
+				{
+					localize.key = key;
+				}
 
-                UILocalize localize = gameObject.GetComponentInChildren<UILocalize>();
-                if (localize != null)
-                {
-                    localize.key = key;
-                }
+				gameObject.SetActive(wasActive);
+			}
+		}
 
-                gameObject.SetActive(wasActive);
-            }
-        }
+		internal static void SetButtonSprite(UIButton button, string sprite)
+		{
+			if (button != null)
+			{
+				button.normalSprite = sprite;
+			}
+		}
 
-        internal static void SetButtonSprite(UIButton button, string sprite)
-        {
-            if (button is not null)
-            {
-                button.normalSprite = sprite;
-            }
-        }
+		internal static void SetTexture(Component component, Texture2D texture)
+		{
+			if (component == null || texture == null)
+			{
+				return;
+			}
 
-        internal static void SetTexture(Component component, Texture2D texture)
-        {
-            if (component is null || texture is null)
-            {
-                return;
-            }
+			UITexture uiTexture = component.GetComponent<UITexture>();
+			if (uiTexture == null)
+			{
+				return;
+			}
 
-            UITexture? uiTexture = component.GetComponent<UITexture>();
-            if (uiTexture is null)
-            {
-                return;
-            }
+			uiTexture.mainTexture = texture;
+		}
 
-            uiTexture.mainTexture = texture;
-        }
+		internal static GameObject GetChild(this GameObject gameObject, string childName)
+		{
+			return string.IsNullOrEmpty(childName) || gameObject == null || gameObject.transform == null || gameObject.transform.FindChild(childName) == null ? null : gameObject.transform.FindChild(childName).gameObject;
+		}
 
-        internal static GameObject? GetChild(this GameObject gameObject, string childName)
-        {
-            return string.IsNullOrEmpty(childName)
-                || gameObject == null
-                || gameObject.transform == null
-                || gameObject.transform.FindChild(childName) == null
-                ? null
-                : gameObject.transform.FindChild(childName).gameObject;
-        }
-
-        internal static void SetUnloadButtonLabel(Panel_Inventory_Examine panel, string localizationKey)
-        {
-            if (panel == null) return;
+		internal static void SetUnloadButtonLabel(Panel_Inventory_Examine panel, string localizationKey)
+		{
+			if (panel == null) return;
 #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
 #pragma warning disable CS8604 // Possible null reference argument.
-            GameObject unloadPanel = GetChild(panel?.m_ExamineWidget?.gameObject, "UnloadRiflePanel");
+			GameObject unloadPanel = GetChild(panel.m_ExamineWidget.gameObject, "UnloadRiflePanel");
 #pragma warning restore CS8604 // Possible null reference argument.
 #pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
-            SetButtonLocalizationKey(gameObject: unloadPanel, localizationKey);
-        }
-    }
+			SetButtonLocalizationKey(gameObject: unloadPanel, localizationKey);
+		}
+	}
 }
