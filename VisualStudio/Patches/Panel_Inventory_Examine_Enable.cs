67fc0d500f082a48b20f41f3dd3c4a207a1d50b9
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2025-05-18 15:34:26 -0300

    Update Panel_Inventory_Examine_Enable.cs

--- VisualStudio/Patches/Panel_Inventory_Examine_Enable.cs
+++ VisualStudio/Patches/Panel_Inventory_Examine_Enable.cs
@@ -3,9 +3,9 @@ namespace FuelManager
 	[HarmonyPatch(typeof(Panel_Inventory_Examine), nameof(Panel_Inventory_Examine.Enable), [typeof(bool), typeof(ComingFromScreenCategory)])]
 	internal class Panel_Inventory_Examine_Enable
 	{
-		private static void Prefix(Panel_Inventory_Examine __instance, bool enable)
+		private static void Prefix(ref Panel_Inventory_Examine __instance, ref bool enable)
 		{
-			if (!__instance.IsPanelPatchable()) return;
+			if (!__instance.IsPanelPatchable(out string reason)) return;
 			if (__instance.m_GearItem == null) return;
 
 			if (!enable) return;
@@ -33,7 +33,7 @@ namespace FuelManager
 					Buttons.SetUnloadButtonLabel(__instance, "GAMEPLAY_BFM_Drain");
 
 					Transform lanternTexture = __instance.m_RefuelPanel.transform.Find("FuelDisplay/Lantern_Texture");
-					Buttons.SetTexture(lanternTexture, Il2Cpp.Utils.GetInventoryIconTexture(__instance.m_GearItem));
+					Buttons.SetTexture(lanternTexture, Utils.GetInventoryIconTexture(__instance.m_GearItem));
 					Main.Logger.Log($"{__instance.m_GearItem.name}: IsFuelItem", FlaggedLoggingLevel.Debug);
 				}
 				catch (Exception e)
