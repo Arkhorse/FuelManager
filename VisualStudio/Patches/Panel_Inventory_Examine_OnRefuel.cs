f2385dc3de7cc8f4fdfed7cdca882fa55fd9396f
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2025-05-18 15:34:24 -0300

    Update Panel_Inventory_Examine_OnRefuel.cs

--- VisualStudio/Patches/Panel_Inventory_Examine_OnRefuel.cs
+++ VisualStudio/Patches/Panel_Inventory_Examine_OnRefuel.cs
@@ -3,9 +3,9 @@ namespace FuelManager
 	[HarmonyPatch(typeof(Panel_Inventory_Examine), nameof(Panel_Inventory_Examine.OnRefuel))]
 	internal class Panel_Inventory_Examine_OnRefuel
 	{
-		private static bool Prefix(Panel_Inventory_Examine __instance)
+		private static bool Prefix(ref Panel_Inventory_Examine __instance)
 		{
-			if (!__instance.IsPanelPatchable()) return true;
+			if (!__instance.IsPanelPatchable(out string reason)) return true;
 			if (__instance.m_GearItem == null) return true;
 
 			GearItem gi = __instance.m_GearItem;
