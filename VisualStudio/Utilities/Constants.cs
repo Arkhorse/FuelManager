6222fb40bca47052d33f9c2c401bb746db86aeda
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2025-05-18 15:33:50 -0300

    Update Constants.cs

--- VisualStudio/Utilities/Constants.cs
+++ VisualStudio/Utilities/Constants.cs
@@ -1,10 +1,20 @@
 ﻿namespace FuelManager
 {
-    internal class Constants
-    {
-        internal static string[] FuelContainerWhiteList { get; } = { "GEAR_GasCan", "GEAR_GasCanFull", "GEAR_JerrycanRusty", "GEAR_LampFuel", "GEAR_LampFuelFull" };
-        internal static string[] REPAIR_HARVEST_GEAR { get; } = { "GEAR_ScrapMetal" };
-        internal static string[] HARVEST_TOOLS { get; } = { "GEAR_Hacksaw" };
-        internal static string[] REPAIR_TOOLS { get; } = { "GEAR_SimpleTools", "GEAR_HighQualityTools" };
-    }
+	public sealed class Constants
+	{
+#pragma warning disable IDE0300
+		public static string[] FuelContainerWhiteList { get; } = { "GEAR_GasCan", "GEAR_GasCanFull", "GEAR_JerrycanRusty", "GEAR_LampFuel", "GEAR_LampFuelFull" };
+		public static string[] REPAIR_HARVEST_GEAR { get; } = { "GEAR_ScrapMetal" };
+		public static string[] HARVEST_TOOLS { get; } = { "GEAR_Hacksaw" };
+		public static string[] REPAIR_TOOLS { get; } = { "GEAR_SimpleTools", "GEAR_HighQualityTools" };
+		/// <summary>
+		/// This checks using known methods to see if the current liquid container is empty
+		/// </summary>
+		/// <param name="a">This is the container liquid volume instance</param>
+		/// <returns><see langword="true"/> if the give <paramref name="a"/> is less than <see cref="ItemLiquidVolume.Zero"/>, otherwise <see langword="false"/></returns>
+		/// <remarks>This must be <c>&lt;</c> as otherwise this thing wont work properly</remarks>
+		public static bool Empty(ItemLiquidVolume a) => a < ItemLiquidVolume.Zero; // must be <, not <=
+
+#pragma warning restore IDE0300
+	}
 }
