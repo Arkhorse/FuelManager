20ef10b6c104a0f7321257e9b846e2f9d1f38a1f
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2026-01-04 14:54:19 -0400

    Create FuelItemData.cs

---
+++ VisualStudio/API/FuelItemData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelManager.API
+{
+	public class FuelItemData : MonoBehaviour
+	{
+		public ItemLiquidVolume CurrentLiters;
+		public ItemLiquidVolume MaxLiters;
+		public bool Empty { get; set; } = false;
+		public bool Full { get; set; } = false;
+	}
+}
